4aae8abee60f4d810018c4b2a257719c34b0a708
Matthew Koch <matthewjameskoch@gmail.com>
2021-09-20 10:01:10 -0400

    url_for

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='Matt')
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return 'This is a test flask app for learning purposes.'
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else: 
+        return redirect(url_for('home'))
 
 
